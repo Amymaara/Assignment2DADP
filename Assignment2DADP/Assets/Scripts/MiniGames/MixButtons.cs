3236d993d18841cad9cac86091dd80f29590600c
afancyfrog <tanyafreese@gmail.com>
2025-09-17 00:51:35 +0200

    Mixing Minigame

---
+++ Assignment2DADP/Assets/Scripts/MiniGames/MixButtons.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MixButtons : MonoBehaviour
+{
+    public enum Directions
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public Directions arrowDirection;
+}
