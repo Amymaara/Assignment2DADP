fb26d3dfc986292db00259a667eef93dc3f0c74a
Amymaara <98781672+Amymaara@users.noreply.github.com>
2025-10-22 12:39:40 +0200

    .

---
+++ Assignment2DADP/Assets/Scripts/UI/HideMarker.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class HideMarker : MonoBehaviour
+{
+    // [SerializeField] private DialogueController dialogueController;
+
+    public GameObject arrowMarker;
+   // public GameObject dialogueBox;
+
+    // checks to see if the dialogue controller & dialogue box is active is scene
+    // public bool IsDialogueOpen => dialogueController && dialogueController.gameObject.activeInHierarchy;
+    public void showArrowMarker()
+    {
+        arrowMarker.SetActive(true);
+    }
+
+    public void hideArrowMarker()
+    {
+        arrowMarker.SetActive(false);
+    }
+
+}
