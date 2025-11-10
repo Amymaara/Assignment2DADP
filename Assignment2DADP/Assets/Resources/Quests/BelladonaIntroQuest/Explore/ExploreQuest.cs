9e30c957aed15cae1eaa3f3236719ab4d5e3e3d1
Amymaara <98781672+Amymaara@users.noreply.github.com>
2025-11-10 18:35:32 +0200

    ink fix

--- Assignment2DADP/Assets/Resources/Quests/BelladonaIntroQuest/Explore/ExploreQuest.cs
+++ Assignment2DADP/Assets/Resources/Quests/BelladonaIntroQuest/Explore/ExploreQuest.cs
@@ -2,22 +2,6 @@ using UnityEngine;
 
 public class ExploreQuest : QuestSteps
 {
-    [Header("Settings")]
-    public float autoCompleteDelay = 1f;
-    [SerializeField] private CandleManager candleManager;
-
-
-    private void Start()
-    {
-        Invoke(nameof(FinishStep), autoCompleteDelay);
-
-    }
-
-    void FinishStep()
-    {
-        Debug.Log("[QuestTest] Completing step now!");
-        FinishQuestStep();
-    }
 
     protected override void SetQuestStepState(string state)
     {
