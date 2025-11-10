163541428ee229fe5756c6111ddde2af8de150c3
Amymaara <98781672+Amymaara@users.noreply.github.com>
2025-11-10 12:42:13 +0200

    intial

--- Assignment2DADP/Assets/Scripts/Tarot/Customer Tarot/TarotPileInteractable.cs
+++ Assignment2DADP/Assets/Scripts/Tarot/Customer Tarot/TarotPileInteractable.cs
@@ -9,13 +9,13 @@ public class TarotPileInteractable : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-       /* Debug.Log("[TarotPile] Interact called");
+        Debug.Log("[TarotPile] Interact called");
 
         if (!spawner) { Debug.LogError("[TarotPile] Missing spawner"); return; }
         if (!tarotManager) { Debug.LogError("[TarotPile] Missing TarotManager"); return; }
         if (!spawner.HasActiveCustomer) { Debug.Log("[TarotPile] No active customer"); return; }
         if (spawner.OrderRevealed) { Debug.Log("[TarotPile] Already revealed for this customer"); return; }
-       */
+       
 
         tarotManager.OpenWithEntry(spawner.CurrentEntry);
     }
